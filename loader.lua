cfee43b166d172aea54237596785056b7355c263
Nolan290908 <nolan1.papp@gmail.com>
2026-09-14 13:41:05 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,17 +1,15 @@
-local DEFAULT_RAW = getgenv().hydroxide_raw or "https://git.fable.bz/zyu/hydroxide/raw/branch/main/"
-
 local gameId = game.GameId
 if gameId == 1087859240 then
     pcall(function()
         loadstring(game:HttpGet(
-            DEFAULT_RAW .. "ROGUE/rogue_ui.lua?nonce="..tostring(math.random()),
+            "https://raw.githubusercontent.com/Nolan290908/Rogue-Lineage-/main/ROGUE/rogue_ui.lua",
             true
         ))()
     end)
 elseif gameId == 7359098240 then
     pcall(function()
         loadstring(game:HttpGet(
-            DEFAULT_RAW .. "ROGUE_BATTLEGROUNDS/rlb.lua?nonce="..tostring(math.random()),
+            "https://raw.githubusercontent.com/Nolan290908/Rogue-Lineage-/main/ROGUE_BATTLEGROUNDS/rlb.lua",
             true
         ))()
     end)
