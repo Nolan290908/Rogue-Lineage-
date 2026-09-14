f17c6c3b6db9fe629a06f5b22294695d047867b0
Nolan290908 <nolan1.papp@gmail.com>
2026-09-14 13:42:36 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,14 +2,14 @@ local gameId = game.GameId
 if gameId == 1087859240 then
     pcall(function()
         loadstring(game:HttpGet(
-            "https://raw.githubusercontent.com/Nolan290908/Rogue-Lineage-/main/ROGUE/rogue_ui.lua",
+            "https://raw.githubusercontent.com/Nolan290908/Rogue-Lineage-/main/rogue_ui.lua",
             true
         ))()
     end)
 elseif gameId == 7359098240 then
     pcall(function()
         loadstring(game:HttpGet(
-            "https://raw.githubusercontent.com/Nolan290908/Rogue-Lineage-/main/ROGUE_BATTLEGROUNDS/rlb.lua",
+            "https://raw.githubusercontent.com/Nolan290908/Rogue-Lineage-/main/rlb.lua",
             true
         ))()
     end)
